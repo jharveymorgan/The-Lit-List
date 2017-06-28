1cef4d6d3810d1c39026b6a9705f67d93c6d0b81
jharveymorgan <jharveymorgan11@gmail.com>
2017-06-28 12:28:59 -0400

    change type of controller for table view

---
+++ The Lit List/Supporting/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  The Lit List
+//
+//  Created by Jordan Harvey-Morgan on 6/28/17.
+//  Copyright © 2017 Jordan Harvey-Morgan. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    // Cells
+    struct TableViewCell {
+        static let listItemCell = "listItemCell"
+    }
+}
