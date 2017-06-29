716f5dde213c3ebc77d18531e698536b3ec38e2d
jharveymorgan <jharveymorgan11@gmail.com>
2017-06-29 14:16:25 -0400

    Add SearchBy view

---
+++ The Lit List/Controllers/SearchByViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchByViewController.swift
+//  The Lit List
+//
+//  Created by Jordan Harvey-Morgan on 6/29/17.
+//  Copyright © 2017 Jordan Harvey-Morgan. All rights reserved.
+//
+
+import UIKit
+
+class SearchByViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - VC Lifecyle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UIViewController.configureBackgroundColor(view: self.view)
+    }
+    
+    
+
+}
